a3bca29f76d1989296e4653e6cfbf721e18eca16
Emily Pisaygnane <episaygnane@me.com>
2022-10-03 15:42:23 -0700

    added Home function so that users go to todolist and non users are sent to auth

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,1 +1,14 @@
 import './Home.css';
+import { useAuth } from '../../context/UserContext';
+import Auth from '../Auth/Auth';
+import TodoList from '../TodoList/TodoList';
+
+export default function Home() {
+  const { currentUser } = useAuth();
+  return (
+    <div>
+      {!currentUser && <Auth />}
+      {currentUser && <TodoList />}
+    </div>
+  );
+}
